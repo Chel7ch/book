72648a125794ed6515ef0cf2d5e050d04b545469
Chel7ch <dmturov@gmail.com>
2021-09-03 20:30:09 +0600

    Configure database sessions

--- src/Controller/Admin/DashboardController.php
+++ src/Controller/Admin/DashboardController.php
@@ -1,22 +1,23 @@
 <?php
-
 namespace App\Controller\Admin;
 
 use App\Entity\Comment;
 use App\Entity\Conference;
-use  App\Controller\Admin\ConferenceCrudController;
-use  App\Controller\Admin\CommentCrudController;
 use EasyCorp\Bundle\EasyAdminBundle\Config\Dashboard;
 use EasyCorp\Bundle\EasyAdminBundle\Config\MenuItem;
 use EasyCorp\Bundle\EasyAdminBundle\Controller\AbstractDashboardController;
-use EasyCorp\Bundle\EasyAdminBundle\Router\AdminUrlGenerator;
 use EasyCorp\Bundle\EasyAdminBundle\Router\CrudUrlGenerator;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use  App\Controller\Admin\ConferenceCrudController;
+use  App\Controller\Admin\CommentCrudController;
+use EasyCorp\Bundle\EasyAdminBundle\Router\AdminUrlGenerator;
 
 class DashboardController extends AbstractDashboardController
 {
-//    #[Route('/admin', name: 'admin')]
+
+
+    #[Route('/admin', name: 'admin')]
     public function index(): Response
     {
         return parent::index();
@@ -36,8 +37,7 @@ class DashboardController extends AbstractDashboardController
     public function configureDashboard(): Dashboard
     {
         return Dashboard::new()
-//            ->setTitle('Book');
-        ->setTranslationDomain('admin');
+            ->setTitle('Book');
     }
 
     public function configureMenuItems(): iterable
