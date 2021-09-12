5723ec1b34052562487309303f50e1043374cdac
Chel7ch <dmturov@gmail.com>
2021-09-12 20:06:27 +0600

    add slug

--- src/Entity/Conference.php
+++ src/Entity/Conference.php
@@ -6,9 +6,12 @@ use App\Repository\ConferenceRepository;
 use Doctrine\Common\Collections\ArrayCollection;
 use Doctrine\Common\Collections\Collection;
 use Doctrine\ORM\Mapping as ORM;
+use Symfony\Bridge\Doctrine\Validator\Constraints\UniqueEntity;
+use Symfony\Component\String\Slugger\SluggerInterface;
 
 /**
  * @ORM\Entity(repositoryClass=ConferenceRepository::class)
+ * @UniqueEntity("slug")
  */
 class Conference
 {
@@ -39,6 +42,11 @@ class Conference
      */
     private $comments;
 
+    /**
+     * @ORM\Column(type="string", length=255, unique=true)
+     */
+    private $slug;
+
     public function __construct()
     {
         $this->comments = new ArrayCollection();
@@ -46,12 +54,19 @@ class Conference
 
     public function __toString(): string
     {
-        return $this->city.' '.$this->year;
+        return $this->city . ' ' . $this->year;
     }
 
     public function getId(): ?int
     {
         return $this->id;
+    }
+
+    public function computeSlug(SluggerInterface $slugger)
+    {
+        if (!$this->slug || '-' === $this->slug) {
+            $this->slug = (string)$slugger->slug((string)$this)->lower();
+        }
     }
 
     public function getCity(): ?string
@@ -132,5 +147,17 @@ class Conference
 //
 //        return $this;
 //    }
+
+    public function getSlug(): ?string
+    {
+        return $this->slug;
+    }
+
+    public function setSlug(string $slug): self
+    {
+        $this->slug = $slug;
+
+        return $this;
+    }
 
 }
